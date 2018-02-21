c37fc3581cc7b0ec9f4578124553d6580a1dacec
Nils Kuhn <nils.kuhn@iteratec.de>
2018-02-21 01:04:54 +0100

    reformatted bash script to run mqtt broker locally

--- no-src/mqtt-broker/run.sh
+++ no-src/mqtt-broker/run.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker run -it -p 1883:1883 -p 9001:9001 -v mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
+
+docker run -d \
+    -p 1883:1883 \
+    -p 9001:9001 \
+    --name mqtt-broker \
+    -v /mosquitto/data \
+    -v /mosquitto/log \
+    -v /home/nkuhn/go/src/iteragit.iteratec.de/traze/goclient/no-src/mqtt-broker/mosquitto.conf:/mosquitto/config/mosquitto.conf \
+    eclipse-mosquitto
+
+# docker run -d -p 1883:1883 -p 9001:9001 --name mqtt-broker -v /mosquitto/data -v /mosquitto/log eclipse-mosquitto
+# docker cp mosquitto.conf mqtt-broker:/mosquitto/config/
